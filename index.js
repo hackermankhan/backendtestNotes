d89e408b3ade6e61c248e43b976bbd11f32bab2a
Khandaker Shayel <kshayel1717@bths.edu>
2022-02-15 00:13:27 -0500

    added validation

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
-  if (!body.content) { //if the content property of the note does not exist
+  if (body.content === undefined) { //if the content property of the note does not exist
     return response.status(400).json({ 
       error: 'content missing' 
     })
@@ -51,17 +51,18 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body 
+  const {content, important} = request.body 
 
   const note = {
-    content: body.content, 
-    important: body.important,
+    content, 
+    important
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, {new: true})
+  Note.findByIdAndUpdate(request.params.id, note, {new: true, runValidators: true, context: 'query'})
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -81,6 +82,9 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } 
+  else if (error.name === 'ValidationError'){
+    return response.status(400).send({error: 'error.message'})
+  }
 
   next(error)
 }
