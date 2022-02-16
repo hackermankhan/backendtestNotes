3dc43eb11837373861794f7a7e2ebde7a4afdcff
Khandaker Shayel <kshayel1717@bths.edu>
2022-02-15 20:21:48 -0500

    restructured files
    MVC

--- models/note.js
+++ models/note.js
@@ -1,28 +1,19 @@
 const mongoose = require('mongoose')
 
 
-const url = process.env.MONGODB_URI
-console.log('connecting to', url)
-mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
 
 //define scheme
 const noteSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        minLength: 5,
-        required: true
-    },
-    date: {
-      type: Date, 
-      required: true
-    },
-    important: Boolean,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  important: Boolean,
 })
 
 noteSchema.set('toJSON', {
